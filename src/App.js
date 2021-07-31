53f855ab4e694a4e15cbce3e5025bee3ba28ff73
jmsmf28 <jmsmf2011@gmail.com>
2021-07-31 13:13:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Bustabit from './components/Bustabit';
+
+function App() {
+  return (
+    <div className="App">
+      <Bustabit />
+    </div>
+  );
+}
+
+export default App;
